ca64a4b5a44b25722ea2b454a8ea98fec0f22d82
Jeffrey-Chung <Cheuk.Chung@uts.edu.au>
2023-02-28 15:12:24 +1100

    UTS website loaded successfully on firefox
    1. Need to install geckodriver on my machine, then link my geckodriver path to my service path 2. Change incognito option from chrome to private for firefox

--- uts_website_test.py
+++ uts_website_test.py
@@ -4,19 +4,22 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.service import Service
 import os
+import geckodriver_autoinstaller
 
-firefox_service = Service('/usr/bin/firefox')
+geckodriver_autoinstaller.install()
+firefox_service = Service('/usr/local/bin/geckodriver')
 firefox_options = Options()
-firefox_options.add_argument("--ignore-certificate-errors")
-firefox_options.add_argument("--incognito")
-firefox_options.add_argument("--headless")
+#firefox_options.add_argument("--ignore-certificate-errors")
+firefox_options.add_argument("--private")
+#firefox_options.add_argument("--headless")
 firefox_options.add_argument("--window-size=1920,1080")
 #driver = webdriver.Chrome(service=s, options=options)
-driver = webdriver.Firefox(service=firefox_service, options=firefox_options)
+driver = webdriver.Firefox(service = firefox_service, options = firefox_options)
 driver.get('https://www.uts.edu.au/')
+driver.open()
 '''
 try:
 
 finally:
     driver.quit()
-'''
+    '''
