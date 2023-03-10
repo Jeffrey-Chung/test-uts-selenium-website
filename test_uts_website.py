3f4a2cd5928f1965e6b86d7c9b3d3037a44879e9
Jeffrey-Chung <Cheuk.Chung@uts.edu.au>
2023-03-10 16:38:31 +1100

    Remove unit test

--- test_uts_website.py
+++ test_uts_website.py
@@ -46,6 +46,7 @@ else:
 left_arrow_key = driver.find_element(By.XPATH, '/html/body/div[1]/div[2]/div[2]/div[3]/div[2]/div/button[1]').click()
 right_arrow_key = driver.find_element(By.XPATH, '/html/body/div[1]/div[2]/div[2]/div[3]/div[2]/div/button[2]').click()
 
+'''
 random_course_index = random.randint(1, 13)
 print("You have tested this course area: " + undergrad_and_postgrad_course_areas[random_course_index-1])
 #Click on Explore Course Areas -> Finds a random course area to choose from
@@ -55,7 +56,7 @@ undergrad_and_postgrad_course_areas_button = driver.find_element(By.XPATH, f'/ht
 #Click on Study Button -> Randomly finds a course to choose from to test
 study_button = driver.find_element(By.XPATH, '/html/body/div[1]/header/div[4]/nav/ul/li[1]/span').click()
 find_a_course_button = driver.find_element(By.XPATH, f'/html/body/div[1]/div[3]/main/ul/li[1]/nav/div/ul/li[{random_course_index}]/a').click()
-
+'''
 #Click on Search Button then close Search Bar
 search_button = driver.find_element("id", 'site-search-toggle').click()
 #search_text = driver.find_element("id", 'edit-search-keys').send_keys("UTS")
