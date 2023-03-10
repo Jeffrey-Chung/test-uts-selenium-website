163f53a1233b1fe5b81c25036de9c3febe68ca49
Jeffrey-Chung <Cheuk.Chung@uts.edu.au>
2023-03-10 21:40:58 +1100

    Re-configure driver settings

--- test_uts_website.py
+++ test_uts_website.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.service import Service
 import random
 
@@ -21,12 +20,13 @@ undergrad_and_postgrad_course_areas = [
 'Transdisciplinary Innovation'
 ]
 
-firefox_service = Service('/usr/local/bin/geckodriver')
-firefox_options = Options()
+#firefox_service = Service('/usr/local/bin/geckodriver')
+firefox_options = webdriver.FirefoxOptions()
 #firefox_options.add_argument("--ignore-certificate-errors")
 firefox_options.add_argument("--private")
-#firefox_options.add_argument("--headless")
-#firefox_options.add_argument("--window-size=1920,1080")
+firefox_options.add_argument("--headless")
+firefox_options.add_argument("--width=1920")
+firefox_options.add_argument("--height=1080")
 #firefox_driver = webdriver.Firefox(service = firefox_service, options = firefox_options)
 
 driver = webdriver.Remote(
@@ -46,7 +46,7 @@ else:
 left_arrow_key = driver.find_element(By.XPATH, '/html/body/div[1]/div[2]/div[2]/div[3]/div[2]/div/button[1]').click()
 right_arrow_key = driver.find_element(By.XPATH, '/html/body/div[1]/div[2]/div[2]/div[3]/div[2]/div/button[2]').click()
 
-'''
+
 random_course_index = random.randint(1, 13)
 print("You have tested this course area: " + undergrad_and_postgrad_course_areas[random_course_index-1])
 #Click on Explore Course Areas -> Finds a random course area to choose from
@@ -56,7 +56,7 @@ undergrad_and_postgrad_course_areas_button = driver.find_element(By.XPATH, f'/ht
 #Click on Study Button -> Randomly finds a course to choose from to test
 study_button = driver.find_element(By.XPATH, '/html/body/div[1]/header/div[4]/nav/ul/li[1]/span').click()
 find_a_course_button = driver.find_element(By.XPATH, f'/html/body/div[1]/div[3]/main/ul/li[1]/nav/div/ul/li[{random_course_index}]/a').click()
-'''
+
 #Click on Search Button then close Search Bar
 search_button = driver.find_element("id", 'site-search-toggle').click()
 #search_text = driver.find_element("id", 'edit-search-keys').send_keys("UTS")
